da678b1721845fad1f85e290a2690880d1e19579
Manny Verma <mannyverma11@gmail.com>
2021-12-08 18:11:03 -0700

    adding more functions

--- js/app.js
+++ js/app.js
@@ -21,9 +21,21 @@ function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
     console.log(`Remember to buy ${item3}`);
   }
 
+  
+  // return function
   function monitorCount(rows, columns) {
     return rows * columns
     };
     const numOfMonitors = monitorCount(5, 4) 
     console.log(numOfMonitors);
 
+    // cost of monitors, helper functions multiple functions
+    function monitorCount(rows, columns) {
+        return rows * columns;
+      }
+      function costOfMonitors(rows, columns){
+        return monitorCount(rows, columns) * 200;
+      }
+      const totalCost = costOfMonitors(5, 4)
+      
+      console.log(totalCost);
