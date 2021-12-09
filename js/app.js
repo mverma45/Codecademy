402f1f66684d95931581f18cdf11253135423ca7
Manny Verma <mannyverma11@gmail.com>
2021-12-08 18:06:17 -0700

    adding functions (#10)

--- js/app.js
+++ js/app.js
@@ -13,3 +13,17 @@ if (runnerAge === 18 && earlyReg === true) {
   console.log('Pleae see the registration desk for more information.')
 }
 
+
+//  assigning a default value
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+    console.log(`Remember to buy ${item1}`);
+    console.log(`Remember to buy ${item2}`);
+    console.log(`Remember to buy ${item3}`);
+  }
+
+  function monitorCount(rows, columns) {
+    return rows * columns
+    };
+    const numOfMonitors = monitorCount(5, 4) 
+    console.log(numOfMonitors);
+
