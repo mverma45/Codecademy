f807d6d12f128a0fa4e6f743e247e48ec20aca8d
Manny Verma <mannyverma11@gmail.com>
2021-12-15 14:54:22 -0700

    commit code

--- js/app.js
+++ js/app.js
@@ -313,3 +313,18 @@ const reportingForDuty = (rank, lastName) => {
   console.log(reportingForDuty('Private', 'Fido'))
   console.log(reportingForDuty('Gen', 'Verma'))
   console.log(reportingForDuty('Col', 'Sanders love me some chicken'))
+
+
+  //rolling the Dice and returning the number as a whole number
+  const rollTheDice = () => {
+    
+    // Math.random() gives us a random number from 0 up to, but not including, 1
+    // We multiplied that by 6 to get a number between 0 and up to, but not including, 6
+    // But since we actually wanted numbers from 1 to 6, inclusive, we added 1
+    // Math.floor() rounds the number so we have a whole number
+      let die1 = Math.floor(Math.random()) * 6 + 1
+      let die2 = Math.floor(Math.random()) * 6 + 1
+      //returns the sum
+      return die1 + die2;
+      console.log(rollTheDice(die1 + die2 ));
+    }
