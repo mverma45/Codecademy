01fa1ae1ddba94717a73d3eee329ade1419bfbc1
Manny Verma <mannyverma11@gmail.com>
2021-12-16 20:01:23 -0700

    adding relationship wip (#25)

--- js/app.js
+++ js/app.js
@@ -403,4 +403,26 @@ return (Math.ceil(imaginaryFriends / 4));
               }
           }
 
-
+          // relationships  
+          const whatRelation = percentSharedDNA => {
+            if (percentSharedDNA === 100) {
+                return 'You are likely identical twins.'
+            }
+            if (percentSharedDNA > 35 && percentSharedDNA < 99) {
+                return 'You are likely parent and child or full siblings.'
+            }
+            if (percentSharedDNA < 14 && percentSharedDNA < 34) {
+                return 'You are likely grandparent and grandchild, aunt/uncle and niece/nephew, or half siblings.'
+            }
+            if (percentSharedDNA > 6 && percentSharedDNA < 13) {
+                return 'You are likely 1st cousins.'
+            }
+            if (percentSharedDNA > 1 && percentSharedDNA < 2) {
+                return 'You are likely 2nd cousins.'
+            }
+            if (percentSharedDNA > 0) {
+                return 'You are likely 3rd cousins'
+            }
+            return 'You are likely not related.'
+        }
+        
