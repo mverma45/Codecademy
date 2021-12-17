e543420a88f9f8baf8d3d18a0f72cf055897a7e9
Manny Verma <mannyverma11@gmail.com>
2021-12-16 19:58:55 -0700

    adding age function (#24)

--- js/app.js
+++ js/app.js
@@ -384,3 +384,23 @@ return (Math.ceil(imaginaryFriends / 4));
       // Uncomment the line below when you're ready to try out your function
       console.log(sillySentence('excited', 'love', 'functions')) 
       console.log(sillySentence('happy', 'live for', 'arrow functions'))
+
+
+        const howOld = (age, year) => {
+          // The following two lines make it so that our function always knows the current year.
+              let dateToday = new Date();
+              let thisYear = dateToday.getFullYear();
+          // It is totally ok if your function used the current year directly!
+            
+              const yearDifference = year - thisYear
+              const newAge = age + yearDifference
+              if (newAge > age) {
+                  return `You will be ${newAge} in the year ${year}`
+              } else if (newAge < 0) {
+                  return `The year ${year} was ${-newAge} years before you were born`
+              } else {
+                  return `You were ${newAge} in the year ${year}`
+              }
+          }
+
+
