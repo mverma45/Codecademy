b1063f5ef5e1532a7c81aea17e43ae16469bbaee
Manny Verma <mannyverma11@gmail.com>
2021-12-17 17:15:12 -0700

    adding faces (#28)

--- js/app.js
+++ js/app.js
@@ -454,4 +454,29 @@ const tipCalculator = (quality, total) => {
   console.log(tipCalculator('okay', 100))
   //should return 20
   
-  // We encourage you to add more function calls of your own to test your code!
+  // Write your function here:
+
+  function toEmoticon(emotion) {
+
+    switch (emotion){
+      case 'shrug':
+        return '|_{"}_|'
+      case 'smiley face':
+        return ':)'
+      case 'frowny face':
+        return ':('
+      case 'winky face':
+        return ';)'
+      case 'heart':
+        return '<3'
+      default:
+        return '|_(* ~ *)_|'
+    }
+    
+    }
+    console.log(toEmoticon("shrug")) 
+    console.log(toEmoticon("smiley face"))
+    console.log(toEmoticon("frowny face"))
+    console.log(toEmoticon("winky face"))
+    console.log(toEmoticon("heart"))
+    console.log(toEmoticon("yoyoyo"))
