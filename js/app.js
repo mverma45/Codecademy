d97121bc6496566db75bbdfcc6900b0e93ee43a6
Manny Verma <mannyverma11@gmail.com>
2021-12-17 20:06:04 -0700

    working on functions (#30)

--- js/app.js
+++ js/app.js
@@ -501,3 +501,26 @@ const isEven = (number) => {
  console.log(isEven(3))
 
  
+ // Create function here 
+//  Create a function numberDigits() that takes the variable x as its only parameter.
+
+// If the variable x is between 0 and 9, return the string 'One digit: N', where N is the value of x. For example, numberDigits(5) would return: 'One digit: 5'
+// If the variable x is between 10 and 99, return the string 'Two digits: N', where N is the value of x. For example, numberDigits(12) would output:
+// 'Two digits: 12'
+// Any other value of x, including negative numbers, return the string 'The number is: N', where N is the value of x. For example, numberDigits(-202) would output:
+// 'The number is: -202'
+  const numberDigits = x => {
+  let numString = '';
+  if (x >= 0 && x <= 9){
+    numString = 'One digit: ' + x;
+  }else if (x >=10 && x <= 99){
+    numString = 'Two digits: ' + x;
+  }else {
+    numString = 'The number is: ' + x;
+  }
+
+  return numString;
+}
+
+console.log(numberDigits(4))
+console.log(numberDigits(-90))
