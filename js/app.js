df6c3a712d952e9404ca52cc3e03bc17585682a1
Manny Verma <mannyverma11@gmail.com>
2022-01-19 09:47:30 -0700

    adding comments to the code (#47)

--- js/app.js
+++ js/app.js
@@ -934,3 +934,41 @@ remotelyDisable(spaceships);
 
 //console logs spaceship
 console.log(spaceships)
+
+
+let spaceship = {
+  crew: {
+  captain: { 
+      name: 'Lily', 
+      degree: 'Computer Engineering', 
+      cheerTeam() { console.log('You got this!') } 
+      },
+  'chief officer': { 
+      name: 'Dan', 
+      degree: 'Aerospace Engineering', 
+      agree() { console.log('I agree, captain!') } 
+      },
+  medic: { 
+      name: 'Clementine', 
+      degree: 'Physics', 
+      announce() { console.log(`Jets on!`) } },
+  translator: {
+      name: 'Shauna', 
+      degree: 'Conservation Science', 
+      powerFuel() { console.log('The tank is full!') } 
+      }
+  }
+}; 
+
+// Write your code below
+// for ... in loop, iterate through the spaceship.crew, console.log list of crew roles and names.
+for (let crewMember in spaceship.crew)
+{
+console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`)
+};
+
+// for ... in loop, iterate through the spaceship.crew, console.log list of crew roles and degrees.
+for (let crewMember in spaceship.crew)
+{
+console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`)
+}
