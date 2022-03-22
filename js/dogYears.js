e7c36a8613fec009ef83ec17c3ccfebde50c316b
Manny Verma <mannyverma11@gmail.com>
2022-03-22 12:57:53 -0600

    Jsproject (#67)
    * adding some work
    
    * adding work
    
    * adding project

---
+++ js/dogYears.js
@@ -0,0 +1,26 @@
+const myAge = 33
+// This is the first 2 years of life.
+let earlyYears = 2
+
+earlyYears *= 10.5
+
+//This is the later years of life.
+laterYears = myAge - 2
+
+//This multiplies the laterYears by 4
+laterYears *= 4
+
+console.log(earlyYears)
+console.log(laterYears)
+
+// My age in dog years
+let myAgeInDogYears = earlyYears + laterYears
+//returns the string in all lower case
+let myName = "MANNY".toLowerCase()
+
+console.log(myAgeInDogYears)
+
+// My age in Dog years sentence
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+)
