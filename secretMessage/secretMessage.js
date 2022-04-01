28ec1601a97638465161c475e51b53167ac2ebf7
Manny Verma <mannyverma11@gmail.com>
2022-04-01 11:23:19 -0600

    Message (#74)
    * adding some work
    
    * adding work
    
    * adding project
    
    * adding files
    
    * adding race
    
    * adding rps
    
    * adding sleepDebtCalc
    
    * adding training_days
    
    * pushing to secretMessage

---
+++ secretMessage/secretMessage.js
@@ -0,0 +1,35 @@
+let secretMessage = [
+  "Learning",
+  "is",
+  "not",
+  "about",
+  "what",
+  "you",
+  "get",
+  "easily",
+  "the",
+  "first",
+  "time,",
+  "it",
+  "is",
+  "about",
+  "what",
+  "you",
+  "can",
+  "figure",
+  "out.",
+  "-2015,",
+  "Chris",
+  "Pine,",
+  "Learn",
+  "JavaScript",
+]
+// console.log(secretMessage.length);
+secretMessage.pop()
+console.log(secretMessage.length)
+secretMessage.push("to", "Program")
+secretMessage.splice(8, 0)
+secretMessage.shift()
+secretMessage.unshift("Programming")
+secretMessage.splice(6, 5, "know,")
+console.log(secretMessage.join(" "))
