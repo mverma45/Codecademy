5836d9af6cdc40eed27f76b3c0df53f66a422c4c
Manny Verma <mannyverma11@gmail.com>
2022-04-03 20:16:51 -0600

    loops

--- loops/loops.js
+++ loops/loops.js
@@ -28,3 +28,14 @@ do {
   cupsAdded++
   console.log(cupsAdded + " cup was added")
 } while (cupsAdded < cupsOfSugarNeeded)
+
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"]
+
+// Write your code below
+for (let i = 0; i < rapperArray.length; i++) {
+  console.log(rapperArray[i])
+  if (rapperArray[i] === "Notorious B.I.G") {
+    break
+  }
+}
+console.log("And if you don't know, now you know.")
