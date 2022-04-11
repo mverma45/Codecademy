d6756fe0cea9c5024fd32de0f3caef96a84e3854
Manny Verma <mannyverma11@gmail.com>
2022-04-11 14:30:16 -0600

    adding energyLevel function (#83)

--- spaceship/spaceship.js
+++ spaceship/spaceship.js
@@ -54,3 +54,16 @@ const robot = {
 robot.provideInfo()
 
 console.log(robot.provideInfo())
+
+const robot = {
+  _model: "1E78V2",
+  _energyLevel: 100,
+  get energyLevel() {
+    if (typeof this._energyLevel === "number") {
+      return "My current energy level is " + this._energyLevel
+    } else {
+      return "System malfunction: cannot retrieve energy level"
+    }
+  },
+}
+console.log(robot.energyLevel)
