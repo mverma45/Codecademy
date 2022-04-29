30f31dae2062b25ff41ba26f34a9ce42b1177415
Manny Verma <mannyverma11@gmail.com>
2022-04-28 18:10:54 -0600

    adding more work

--- Assignments/errorsInCode/script.js
+++ Assignments/errorsInCode/script.js
@@ -31,3 +31,21 @@ function doubleString(string) {
 }
 // Should return 'echoecho'
 console.log("echo".repeat(2))
+
+//using stackoverflow.com/
+
+// Returns whether or not the provided string contains a substring of "cake" in it.
+function containsCake(string) {
+  return string.includes("cake")
+}
+// Should return true
+console.log(
+  "containsCake('I think cake is my soul mate.') returns: " +
+    containsCake("I think cake is my soul mate.")
+)
+
+// Should return false
+console.log(
+  "containsCake('Pie is certainly the coolest dessert.') returns: " +
+    containsCake("Pie is certainly the coolest dessert.")
+)
