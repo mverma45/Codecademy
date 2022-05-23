3762eb3589abe199c0350df91fdb45e53ccbd17e
Manny Verma <mannyverma11@gmail.com>
2022-05-22 19:06:10 -0600

    adding sqrt function

---
+++ Challenges/sqrt/script.js
@@ -0,0 +1,17 @@
+const numbers = [2, 7, 9, 171, 52, 33, 14]
+
+const toSquare = num => num * num
+
+// Write your code here:
+const squareNums = arr => arr.map(n => n * n)
+
+// const numbers = [2, 7, 9, 171, 52, 33, 14]
+
+// const toSquare = num => num * num
+
+// // Write your code here:
+// const squareNums = numbers.map(numbers => {
+//   return Math.sqrt(numbers)
+// })
+
+// console.log(squareNums)
