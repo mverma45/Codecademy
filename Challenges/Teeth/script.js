0de5812ee85edb3393cc54e7efb7f1127a2be221
Manny Verma <mannyverma11@gmail.com>
2022-05-31 14:33:23 -0600

    adding teeth

---
+++ Challenges/Teeth/script.js
@@ -0,0 +1,23 @@
+const speciesArray = [
+  { speciesName: "shark", numTeeth: 50 },
+  { speciesName: "dog", numTeeth: 42 },
+  { speciesName: "alligator", numTeeth: 80 },
+  { speciesName: "human", numTeeth: 32 },
+]
+
+// Write your code here:
+
+const sortSpeciesByTeeth = arr =>
+  arr.sort(
+    (speciesObj1, speciesObj2) => speciesObj1.numTeeth > speciesObj2.numTeeth
+  )
+
+// Feel free to comment out the code below when you're ready to test your function!
+
+console.log(sortSpeciesByTeeth(speciesArray))
+
+// Should print:
+// [ { speciesName: 'human', numTeeth: 32 },
+//   { speciesName: 'dog', numTeeth: 42 },
+//   { speciesName: 'shark', numTeeth: 50 },
+//   { speciesName: 'alligator', numTeeth: 80 } ]
