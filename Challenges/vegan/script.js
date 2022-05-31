a2b9031eaf765dcf9adfa21ab55355ac580e34c1
Manny Verma <mannyverma11@gmail.com>
2022-05-31 14:15:35 -0600

    adding vegan

---
+++ Challenges/vegan/script.js
@@ -0,0 +1,15 @@
+// Write your code here:
+const isTheDinnerVegan = arr => arr.every(food => food.source === "plant")
+
+// Feel free to comment out the code below to test your function
+
+const dinner = [
+  { name: "hamburger", source: "meat" },
+  { name: "cheese", source: "dairy" },
+  { name: "ketchup", source: "plant" },
+  { name: "bun", source: "plant" },
+  { name: "dessert twinkies", source: "unknown" },
+]
+
+console.log(isTheDinnerVegan(dinner))
+// Should print false
