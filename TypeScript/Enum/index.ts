c5f45cc1eabb2c38b00453712ccc87d60ab7dfe2
Manny Verma <mannyverma11@gmail.com>
2022-06-22 18:05:07 -0600

    adding enum

--- TypeScript/Enum/index.ts
+++ TypeScript/Enum/index.ts
@@ -22,3 +22,66 @@ const ordersArrayTS: [Pet, number][] = [
   [Pet.Hamster, 2],
   [Pet.Chinchilla, 50],
 ]
+
+let petOnSale = "chinchilla"
+let ordersArray = [
+  ["rat", 2],
+  ["chinchilla", 1],
+  ["hamster", 2],
+  ["chinchilla", 50],
+]
+
+// Write your code below:
+
+enum Pet {
+  Hamster = "HAMSTER",
+  Rat = "RAT",
+  Chinchilla = "CHINCHILLA",
+  Tarantula = "TARANTULA",
+}
+
+const petOnSaleTS: Pet = Pet.Chinchilla
+
+const ordersArrayTS: [Pet, number][] = [
+  [Pet.Rat, 2],
+  [Pet.Chinchilla, 1],
+  [Pet.Hamster, 2],
+  [Pet.Chinchilla, 50],
+]
+
+ordersArrayTS.push(["HAMSTER", 1])
+
+//in the enum Pet statement, you cannot use 'HAMSTER' as the enum value. You must use Pet.Hamster instead.
+
+function sayHappyBirthdayWithObject(personObject: {
+  name: string
+  age: number
+  giftWish: string
+  success: boolean
+}) {
+  let output = ""
+  output += "Happy Birthday " + personObject.name + "! "
+  output += "You are now " + personObject.age + " years old! "
+  output +=
+    "Your birthday wish was to receive " +
+    personObject.giftWish +
+    ". And guess what? You will "
+  if (!personObject.success) {
+    output += "not "
+  }
+  output += "receive it! \n"
+  console.log(output)
+}
+
+let birthdayBabies: {
+  name: string
+  age: number
+  giftWish: string
+  success: boolean
+}[] = [
+  { name: "Liam", age: 0, giftWish: "karate skills", success: false },
+  { name: "Olivia", age: 0, giftWish: "a bright future", success: true },
+  { name: "Ava", age: 0, giftWish: "$0.25", success: true },
+]
+
+birthdayBabies.forEach(sayHappyBirthdayWithObject)
