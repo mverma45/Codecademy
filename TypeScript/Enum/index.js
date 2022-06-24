8555220791ae63f76503251acddbab605cc8ece9
Manny Verma <mannyverma11@gmail.com>
2022-06-24 15:06:58 -0600

    adding-generic-enum

--- TypeScript/Enum/index.js
+++ TypeScript/Enum/index.js
@@ -120,3 +120,23 @@ let anotherFamily: Family<Dog> = {
     { name: "Puppenator", tailWagSpeed: 0.01 },
   ],
 }
+
+// Generic Functions
+
+function getFilledArray<T>(value: T, n: number): T[] {
+  return Array(n).fill(value)
+}
+
+let stringArray: string[]
+let numberArray: number[]
+let personArray: { name: string, age: number }[]
+let coordinateArray: [number, number][]
+
+// Write your code below:
+stringArray = getFilledArray < string > ("hi", 6)
+numberArray = getFilledArray < number > (9, 6)
+personArray =
+  getFilledArray <
+  { name: string, age: number } >
+  ({ name: "J. Dean", age: 24 }, 6)
+coordinateArray = getFilledArray < [number, number] > ([3, 4], 6)
