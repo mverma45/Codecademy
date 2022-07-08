ef1d8b98d7662855134125d979f8436bcfe07688
Manny Verma <mannyverma11@gmail.com>
2022-07-07 19:41:47 -0600

    Code cc (#123)
    * adding generic-enum
    
    * adding-generic-enum
    
    * adding codecademy stuff
    
    * pushing unions

--- TypeScript/Unions/index.ts
+++ TypeScript/Unions/index.ts
@@ -27,3 +27,88 @@ formatValue("Hiya")
 formatValue(42)
 
 // Inferred Union Return Types
+
+// Declare a variable named userData that stores the returned value of createUser(). Then, go back and provide userData with the correct union type.
+type User = {
+  id: number
+  username: string
+}
+
+function createUser() {
+  const randomChance = Math.random() >= 0.5
+
+  if (randomChance) {
+    return { id: 1, username: "nikko" }
+  } else {
+    return "Could not create a user."
+  }
+}
+
+const userData: string | User = createUser()
+
+// Unions and Arrays
+// Look at the definition of formatListings(), then define a union array type on the listings parameter.
+
+function formatListings(listings: (string | number)[]) {
+  return listings.map(listing => {
+    if (typeof listing === "string") {
+      return listing.toUpperCase()
+    }
+
+    if (typeof listing === "number") {
+      return `$${listing.toLocaleString()}`
+    }
+  })
+}
+
+const result = formatListings([
+  "123 Main St",
+  226800,
+  "580 Broadway Apt 4a",
+  337900,
+])
+
+console.log(result)
+
+// Common Key Value Pairs
+//  adding a type to the getFriendNameFromEvent()’s event parameter. Use a union to allow event to be a Like or a Share. In the Share type, add a displayName of type string, just like on the Like type.
+
+type Like = {
+  username: string
+  displayName: string
+}
+
+type Share = {
+  username: string
+  displayName: string
+}
+
+function getFriendNameFromEvent(event: Like | Share) {
+  return event.displayName || event.username
+}
+
+const newEvent = {
+  username: "vkrauss",
+  displayName: "Veronica Krauss",
+}
+
+const friendName = getFriendNameFromEvent(newEvent)
+
+console.log(`You have an update from ${friendName}.`)
+
+// Unions with Literal Types
+//
+type Status = "idle" | "downloading" | "complete"
+
+function downloadStatus(status: Status) {
+  if (status === "idle") {
+    console.log("Download")
+  }
+  if (status === "downloading") {
+    console.log("Downloading...")
+  }
+  if (status === "complete") {
+    console.log("Your download is complete!")
+  }
+  return downloadStatus("idle")
+}
