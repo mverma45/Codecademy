5a50f9f091d4f3a32335b9ede47a3ada3cd4a7d4
Manny Verma <mannyverma11@gmail.com>
2022-07-14 14:59:38 -0600

    pushing code up

--- TypeScript/Enum/index.js
+++ TypeScript/Enum/index.js
@@ -65,3 +65,90 @@ var birthdayBabies = [
 birthdayBabies.forEach(sayHappyBirthdayWithObject)
 var codecademyCoordinates = [40, 43.2, "N", 73, 59.8, "W"]
 var bermudaTCoordinates = [25, 0, "N", 71, 0, "W"]
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
+function wrongAdd(a, b) {
+  return a + b + ""
+}
+
+// Add your function type below:
+type OperatorFunction = (a: number, b: number) => number
+// Math Tutor Function That Accepts a Callback
+function mathTutor(operationCallback: OperatorFunction) {
+  console.log("Let's learn how to", operationCallback.name, "!")
+  let value25 = operationCallback(2, 5)
+  console.log(
+    "When we",
+    operationCallback.name,
+    "2 and 5, we get",
+    value25,
+    "."
+  )
+  console.log(
+    "When we",
+    operationCallback.name,
+    value25,
+    "and 7, we get",
+    operationCallback(value25, 7),
+    "."
+  )
+  console.log("Now fill out this worksheet.")
+}
+
+// Call your functions below:
+mathTutor(multiply)
+mathTutor(wrongAdd)
+
+//Generic Types
+
+type Human = { name: string, job: string }
+type Dog = { name: string, tailWagSpeed: number }
+
+type Family<T> = {
+  parents: [T, T],
+  mate: T,
+  children: T[],
+}
+//Do not change the code above this line.
+
+//Provide type annotations for the variables below:
+let theFamily: Family<number> = {
+  parents: [3, 4],
+  mate: 9,
+  children: [5, 30, 121],
+}
+
+let someFamily: Family<boolean> = {
+  parents: [true, true],
+  mate: false,
+  children: [false, false, true, true],
+}
+
+let aFamily: Family<Human> = {
+  parents: [
+    { name: "Mom", job: "software engineer" },
+    { name: "Dad", job: "coding engineer" },
+  ],
+  mate: { name: "Matesky", job: "engineering coder" },
+  children: [{ name: "Babesky", job: "none" }],
+}
+
+let anotherFamily: Family<Dog> = {
+  parents: [
+    { name: "Momo", tailWagSpeed: 3 },
+    { name: "Dado", tailWagSpeed: 100 },
+  ],
+  mate: { name: "Cheems", tailWagSpeed: 7 },
+  children: [
+    { name: "Puppin", tailWagSpeed: 0.001 },
+    { name: "Puppenaut", tailWagSpeed: 0.0001 },
+    { name: "Puppenator", tailWagSpeed: 0.01 },
+  ],
+}
