3aa72abe3f3dc8806ed1250f764e465ea5d1cbb7
Manny Verma <mannyverma11@gmail.com>
2022-07-21 19:39:23 -0600

    adding respond events to self driving car

--- TypeScript/Projects/Self_Driving_Car/index.ts
+++ TypeScript/Projects/Self_Driving_Car/index.ts
@@ -1,20 +1,26 @@
-import { getObstacleEvents } from './computer-vision';
+import { getObstacleEvents } from "./computer-vision"
 
 interface AutonomousCar {
-  .isRunning: boolean
+  isRunning?: boolean
+  respond: (events: Events) => void
 }
-
 interface AutonomousCarProps {
-  .isRunning?:boolean;
+  isRunning?: boolean
+}
+interface Events {
+  [i: string]: boolean
 }
 
 class Car implements AutonomousCar {
-  .isRunning;
-  constructor(props)
-constructor(props: AutonomousCarProps){
-  this.isRunning = .isRunning
+  isRunning
+  constructor(props: AutonomousCarProps) {
+    this.isRunning = props.isRunning
+  }
+  respond(events: Events) {
+    if (!this.isRunning) {
+      return console.log("Car is off")
+    }
+  }
 }
-}
-const autonomousCar = new Car({.isRunning: 'true'});
-})
-console.log(.isRunning)
+const autonomousCar = new Car({ isRunning: true })
+autonomousCar.respond(getObstacleEvents())
