9dfad82d6344e5a414f3e6349780d1910c7f034c
Manny Verma <mannyverma11@gmail.com>
2022-07-26 09:00:34 -0600

    adding self driving car

--- TypeScript/Projects/Self_Driving_Car/index.js
+++ TypeScript/Projects/Self_Driving_Car/index.js
@@ -1,5 +1,6 @@
 "use strict";
 exports.__esModule = true;
+var computer_vision_1 = require("./computer-vision");
 var SteeringControl = /** @class */ (function () {
     function SteeringControl() {
     }
@@ -7,22 +8,34 @@ var SteeringControl = /** @class */ (function () {
         console.log("Executing: ".concat(command));
     };
     SteeringControl.prototype.turn = function (direction) {
-        this.execute("Turn: ".concat(direction));
+        this.execute("Turn ".concat(direction));
     };
     return SteeringControl;
 }());
 var Car = /** @class */ (function () {
     function Car(props) {
         this.isRunning = props.isRunning;
+        this.steeringControl = props.steeringControl;
     }
     Car.prototype.respond = function (events) {
+        var _this = this;
         if (!this.isRunning) {
             return console.log("Car is off");
         }
+        Object.keys(events).forEach(function (eventKey) {
+            if (!events[eventKey]) {
+                return;
+            }
+            if (eventKey === "ObstacleLeft") {
+                _this.steeringControl.turn("right");
+            }
+            if (eventKey === "ObstacleRight") {
+                _this.steeringControl.turn("left");
+            }
+        });
     };
     return Car;
 }());
 var steering = new SteeringControl();
-var autonomousCar = new Car({ isRunning: true });
-steering.turn("right");
-// autonomousCar.respond(getObstacleEvents())
+var autonomousCar = new Car({ isRunning: true, steeringControl: steering });
+autonomousCar.respond((0, computer_vision_1.getObstacleEvents)());
--- TypeScript/Projects/Self_Driving_Car/index.ts
+++ TypeScript/Projects/Self_Driving_Car/index.ts
@@ -6,6 +6,7 @@ interface AutonomousCar {
 }
 interface AutonomousCarProps {
   isRunning?: boolean
+  steeringControl: Steering
 }
 interface Events {
   [i: string]: boolean
@@ -22,25 +23,40 @@ class SteeringControl implements Steering {
     console.log(`Executing: ${command}`)
   }
   turn(direction: string) {
-    this.execute(`Turn: ${direction}`)
+    this.execute(`Turn ${direction}`)
   }
 }
 
 class Car implements AutonomousCar {
   isRunning
+  steeringControl
+
   constructor(props: AutonomousCarProps) {
     this.isRunning = props.isRunning
+    this.steeringControl = props.steeringControl
   }
   respond(events: Events) {
     if (!this.isRunning) {
       return console.log("Car is off")
     }
+    Object.keys(events).forEach(eventKey => {
+      if (!events[eventKey]) {
+        return
+      }
+
+      if (eventKey === "ObstacleLeft") {
+        this.steeringControl.turn("right")
+      }
+
+      if (eventKey === "ObstacleRight") {
+        this.steeringControl.turn("left")
+      }
+    })
   }
 }
 
 const steering = new SteeringControl()
 
-const autonomousCar = new Car({ isRunning: true })
+const autonomousCar = new Car({ isRunning: true, steeringControl: steering })
 
-steering.turn("right")
-// autonomousCar.respond(getObstacleEvents())
+autonomousCar.respond(getObstacleEvents())
