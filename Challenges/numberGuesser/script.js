d7848b1dc953f25ae32f6693c62236feef6a91ab
Manny Verma <mannyverma11@gmail.com>
2023-02-07 19:26:13 -0700

    adding number guesser

---
+++ Challenges/numberGuesser/script.js
@@ -0,0 +1,13 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+function generateTarget() {
+  return  Math.floor(Math.random() * 9);
+}
+
+function compareGuesses(humanGuessInput, computerGuesses, secretTargetNumber) {
+    
+}
