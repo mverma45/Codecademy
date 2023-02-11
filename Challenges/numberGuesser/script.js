3f040de231df7d5baad896e5a12a1c52ad14b438
Manny Verma <mannyverma11@gmail.com>
2023-02-10 18:25:30 -0700

    adding numberGuesser files

--- Challenges/numberGuesser/script.js
+++ Challenges/numberGuesser/script.js
@@ -5,9 +5,23 @@ let currentRoundNumber = 1;
 // Write your code below:
 
 function generateTarget() {
-  return  Math.floor(Math.random() * 9);
+  return Math.floor(Math.random() * 10);
 }
+function compareGuesses(humanGuessInput, computerGuessDisplay, targetNumberDisplay) {
+  const humanGuess = Math.abs(targetNumberDisplay - humanGuess);
+  const computerGuess = Math.abs(targetNumberDisplay - computerGuess);
 
-function compareGuesses(humanGuessInput, computerGuesses, secretTargetNumber) {
+  return humanGuess <= computerGuessDisplay; 
+
+  const updateScore = winner => {
+    if (winner === "human") {
+      humanScoreDisplay++;
+    } else if (winner === "computer"){
+      computerScoreDisplay++;
+    }
+
+    const advanceRound = () =>
+      nextRoundButton++;
     
+  }
 }
