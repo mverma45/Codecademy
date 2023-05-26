ad5096f74f5fabe97822f4df8001686cbf41d064
Manny Verma <mannyverma11@gmail.com>
2023-05-26 17:50:53 -0600

    movie finder

--- Challenges/FilmFinder/public/script.js
+++ Challenges/FilmFinder/public/script.js
@@ -19,18 +19,54 @@ const getGenres = async () => {
   }
 };
 
-const getMovies = () => {
-  const selectedGenre = getSelectedGenre();
+const getMovies = async () => {
+      const selectedGenre = getSelectedGenre();
+      const discoverMovieEndpoint =  '/discover/movie';
+      const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
+      const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
+
+  try {
+    const response = await fetch(urlToFetch)
+    if (response.ok){
+      const jsonResponse = await response.json();
+      const movies = jsonResponse.results;
+      return movies;
+    }
+  } catch (error) {
+    console.log(error)
+  }
 };
 
-const getMovieInfo = () => {};
+const getMovieInfo = async (movie) => {
+  const movieId = movie.id
+  const movieEndpoint = `/movie/${movieId}`;
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${movieEndpoint}${requestParams}`;
+
+  try {
+      const response = await fetch(urlToFetch)
+      if (response.ok){
+        const jsonResponse = await response.json();
+        const movieInfo =  jsonResponse;
+        return movieInfo;
+      }
+  } catch(error){
+    console.log(error)
+  }
+};
 
 // Gets a list of movies and ultimately displays the info of a random movie from the list
-const showRandomMovie = () => {
+const showRandomMovie = async () => {
   const movieInfo = document.getElementById("movieInfo");
   if (movieInfo.childNodes.length > 0) {
     clearCurrentMovie();
   }
+
+      const movies = await getMovies();
+      const randomMovie = getRandomMovie(movies);
+      const info = await getMovieInfo(randomMovie)
+      displayMovie(info);
+
 };
 
 getGenres().then(populateGenreDropdown);
